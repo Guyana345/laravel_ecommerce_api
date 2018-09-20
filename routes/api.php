ee2d85ecb68fea7c921525d84be2318840118921
Tafadzwa Makuyana <tafadzwa.makuyana@students.stamford.edu>
2018-09-20 21:19:28 +0700

    migration created for product and review

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//api route for proucts
 Route::apiResource('/products', 'ProductController');
 
+//api route for product reviews
 Route::group(['prefix'=>'products'], function(){
 
     Route::apiResource('/{product}/reviews','ReviewController');
